9ca80b6893f872000e3cdba6e14280f70a2fd484
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2026-01-29 15:00:25 -0300

    add style to card (incompleto)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,21 @@
+import './card.css'
+
 export default function Card({nome, imgSrc, imgAlt, celular, email}) {
     return (
-        <div>
-            <h3>{nome}</h3>
-            <img
-            src={imgSrc}
-            alt={imgAlt}
-            />
-            <p>{celular}</p>
-            <p>{email}</p>
+        <div className='card'>
+            <div className='top'>
+                <h3 className='name'>{nome}</h3>
+            </div>
+            <div>
+                <img className='circle-img'
+                    src={imgSrc}
+                    alt={imgAlt}
+                />
+            </div>
+            <div className='bottom'>
+                <p>{celular}</p>
+                <p>{email}</p>
+            </div>
         </div>
     )
 }
