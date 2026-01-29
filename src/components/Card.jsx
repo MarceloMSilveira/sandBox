2c1b79c10fdbf1680e153e5df70a58e6d7d40d55
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2026-01-29 15:45:48 -0300

    fix img position

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,16 +5,14 @@ export default function Card({nome, imgSrc, imgAlt, celular, email}) {
         <div className='card'>
             <div className='top'>
                 <h3 className='name'>{nome}</h3>
-            </div>
-            <div>
                 <img className='circle-img'
                     src={imgSrc}
                     alt={imgAlt}
                 />
             </div>
             <div className='bottom'>
-                <p>{celular}</p>
-                <p>{email}</p>
+                <p className='info'>{celular}</p>
+                <p className='info'>{email}</p>
             </div>
         </div>
     )
