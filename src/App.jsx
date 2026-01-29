50f74ba6cfc22ca4bb18cfddbffe52e647b16860
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2026-01-29 09:40:15 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Basic React App</h1>
+      <p>Este é um App básico para iniciar projetos com React!</p>
+    </>
+  )
+}
+
+export default App
