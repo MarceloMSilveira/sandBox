4653f8a323c169d42847ee729ca28979ea074d2e
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2026-01-29 10:23:02 -0300

    passando props para um comp funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,34 @@ import Card from './components/Card'
 
 function App() {
   const dadosDosContatos = [
-    {
-      nome: ,
-      img: {
-        src: , 
-        alt:
-      }, 
-      celular: ,
-      email: 
-    }
-  ]
+  {
+    nome: "Beyonce",
+    img: {
+      src: "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+      alt: "avatar_img"
+    },
+    celular: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    nome: "Jack Bauer",
+    img: {
+      src: "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+      alt: "avatar_img"
+    },
+    celular: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    nome: "Chuck Norris",
+    img: {
+      src: "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+      alt: "avatar_img"
+    },
+    celular: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
 
   return (
     <>
@@ -20,7 +38,13 @@ function App() {
       <div>
         <h2>My Contacts</h2>
 
-        <Card nome='Falcão'/>
+        <Card 
+          nome = {dadosDosContatos[0].nome}
+          imgSrc = {dadosDosContatos[0].img.src}
+          imgAlt = {dadosDosContatos[0].img.alt}
+          celular = {dadosDosContatos[0].celular}
+          email = {dadosDosContatos[0].email}
+        />
 
         <h3>Jack Bauer</h3>
         <img
