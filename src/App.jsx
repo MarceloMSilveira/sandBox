cb5ee674f12b7b9e5ca81f57c163efa2e7fc6334
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2026-01-29 14:29:01 -0300

    solucao usando a função map

--- src/App.jsx
+++ src/App.jsx
@@ -36,31 +36,21 @@ function App() {
     <>
       <h1>Props Study App</h1>
       <div>
-        <h2>My Contacts</h2>
-
-        <Card 
-          nome = {dadosDosContatos[0].nome}
-          imgSrc = {dadosDosContatos[0].img.src}
-          imgAlt = {dadosDosContatos[0].img.alt}
-          celular = {dadosDosContatos[0].celular}
-          email = {dadosDosContatos[0].email}
-        />
-
-        <h3>Jack Bauer</h3>
-        <img
-          src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-          alt="avatar_img"
-        />
-        <p>+987 654 321</p>
-        <p>jack@nowhere.com</p>
-
-        <h3>Chuck Norris</h3>
-        <img
-          src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-          alt="avatar_img"
-        />
-        <p>+918 372 574</p>
-        <p>gmail@chucknorris.com</p>
+        <h2>My Contacts:</h2>
+        {
+          dadosDosContatos.map(
+            contato=>{
+              const card = <Card
+                nome = {contato.nome}
+                imgSrc = {contato.img.src}
+                imgAlt = {contato.img.alt}
+                celular = {contato.celular}
+                email = {contato.email}
+              />
+              return card;
+            }
+          )
+        }
       </div>
     </>
   )
