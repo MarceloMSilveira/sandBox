98dabd9bfe25ba8e2c57f1e4fbb27da409ffc69f
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2026-01-31 16:08:48 -0300

    conditional rendering ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Login from './components/Login'
 
 function App() {
+  const isLogged = true;
+
   return (
     <div className="container">
       <h1>Hello</h1>
-      <Login />
+      {!isLogged && <Login />}
     </div>
   )
 }
