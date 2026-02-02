4e243a8525b7a8cb8801feb8ef1983369f17cd41
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2026-02-02 06:03:40 -0300

    done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,24 @@
 import Input from "./Input"
+import { useState } from "react"
 
 export default function Login() {
+  
+  let [isMouseOver, setIsMouseOver] = useState(false)
+  
+  function mouseOver() {
+    setIsMouseOver(true)
+  }
+
+  function mouseLeave() {
+    setIsMouseOver(false)
+  }
+
   return (
     <div>
       <h1>Hello:</h1>
       <form className="form">
         <Input type="text" placeholder="What's your name?"/>
-        <button type="submit">Submit</button>
+        <button  style={{backgroundColor: isMouseOver ? 'black' : 'white'}} onMouseOver={mouseOver} onMouseLeave={mouseLeave} type="submit">Submit</button>
       </form>
     </div>
   )
